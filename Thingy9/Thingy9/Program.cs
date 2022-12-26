1c474845bebac57dc5fcfb6cbbf49cad5ab1493f
golunkater <katerinagolunova@gmail.com>
2022-12-26 16:54:49 +0500

    fgfd

---
+++ Thingy9/Thingy9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////for (int i = 10; i < 1000000000; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Console.WriteLine(MakeNumberToThePowerOf(3,3));
+            
+            Console.ReadKey();
+        }
+
+        static int MakeNumberToThePowerOf(int number, int degree)
+        {
+            int num = number;
+            for (int i = 1; i < degree; i++)
+            {
+                num = num * number;
+            }
+            return num;
+        }
+    }
+}
