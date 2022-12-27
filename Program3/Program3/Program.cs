0ef38a97a72f55a99e812c03b20b3c687fa8e23a
golunkater <katerinagolunova@gmail.com>
2022-12-27 13:23:06 +0500

    1st version of controlWork

---
+++ Program3/Program3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(IsNumberSimple(n));
+
+            Console.ReadKey();
+        }
+
+        static bool IsNumberSimple(int number)
+        {
+            bool flag = true;
+            if (((number % 2 == 0) && (number != 2)) || ((number % 3 == 0) && (number != 3)) || ((number % 5 == 0) && (number != 5)) || ((number % 7 == 0) && (number != 7)))
+            {
+                flag = false;
+            }
+            else
+            {
+                for (int i = 2; i <= (number / 2); i++)
+                {
+                    if (number % (i + 1) == 0)
+                    {
+                        flag = false;
+                    }
+                }
+            }
+            return flag;          
+        }
+    }
+}
